83c87dd78ddfdc15d119d28eeb622606657c177b
Dinesh Tamang <dineshtamang7263@gmail.com>
2023-02-28 18:26:46 +0530

    updated meta tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,15 +39,33 @@ const IndexPage: React.FC<IndexPageProps> = ({ inputRef }) => {
   return (
     <>
       <Head>
-        <title>{config.title}</title>
-      </Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="shortcut icon" href="../assets/favicon.jpg" type="image/x-icon" />
 
-      <Script src="http://192.168.12.105:3000/hook.js" strategy="lazyOnload" />
+        <title>Dinesh Tamang | AWS Solution Architect | Mumbai, India</title>
+        <meta name="description" content="Dinesh Tamang student at Mumbai University, currently in fourth year B.E. Computer Science. Technical skill includes programming in python, java, c++, typescript, Node.js and Databases such as
+        sql and Nosql. and i am also familiar with Web Development, Kubernetes, Docker, Ansible, terraform, AWS etc." />
+        <meta name="keywords" content="Dinesh Tamang, Computer Science, AWS Solution Architect, Cloud Engineer, Backend Engineer, Portfolio website, Dinesh Rambahadur Tamang" />
+        <meta name="author" content="Dinesh Tamang" />
+        <meta name="robots" content="index, follow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="Dinesh Tamang | AWS Solution Architect | Mumbai, India" />
+        <meta property="og:description" content="Dinesh Tamang student at Mumbai University, currently in fourth year B.E. Computer Science. Technical skill includes programming in python, java, c++, typescript, Node.js and Databases such as
+        sql and Nosql. and i am also familiar with Web Development, Kubernetes, Docker, Ansible, terraform, AWS etc." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.dineshtamang.tech/" />
+        <meta property="og:image" content="https://drive.google.com/file/d/10-0Y76IHGnTIA2_5aEli-ey3cBpTUse3/view?usp=sharing" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Dinesh Tamang | AWS Solution Architect | Mumbai, India" />
+        <meta name="twitter:description" content="Dinesh Tamang student at Mumbai University, currently in fourth year B.E. Computer Science. Technical skill includes programming in python, java, c++, typescript, Node.js and Databases such as
+        sql and Nosql. and i am also familiar with Web Development, Kubernetes, Docker, Ansible, terraform, AWS etc." />
+	      <meta name="twitter:image" content="https://pbs.twimg.com/profile_images/1130796628997156864/HFXo5m91_400x400.jpg" />
+      </Head>
 
       <div className="p-8 overflow-hidden h-full border-2 rounded border-light-yellow dark:border-dark-yellow">
         <div ref={containerRef} className="overflow-y-auto h-full">
           <History history={history} />
-
           <Input
             inputRef={inputRef}
             containerRef={containerRef}
