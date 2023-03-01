73bd08013d29b8141a761bcaef5d1d8f341c66bd
Dinesh Tamang <dineshtamang7263@gmail.com>
2023-03-01 22:00:24 +0530

    updated favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,7 +41,7 @@ const IndexPage: React.FC<IndexPageProps> = ({ inputRef }) => {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="theme-color" content="#000000" />
-        <link rel="shortcut icon" href="favicon.jpg" type="image/x-icon" />
+        <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
 
         <title>Dinesh Tamang | AWS Solution Architect | Mumbai, India</title>
         <meta name="description" content="Dinesh Tamang student at Mumbai University, currently in fourth year B.E. Computer Science. Technical skill includes programming in python, java, c++, typescript, Node.js and Databases such as
@@ -54,7 +54,7 @@ const IndexPage: React.FC<IndexPageProps> = ({ inputRef }) => {
         <meta property="og:description" content="Dinesh Tamang student at Mumbai University, currently in fourth year B.E. Computer Science. Technical skill includes programming in python, java, c++, typescript, Node.js and Databases such as
         sql and Nosql. and i am also familiar with Web Development, Kubernetes, Docker, Ansible, terraform, AWS etc." />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.dineshtamang.tech/" />
+        <meta property="og:url" content="https://www.dineshtamang.tech" />
         <meta property="og:image" content="https://drive.google.com/file/d/10-0Y76IHGnTIA2_5aEli-ey3cBpTUse3/view?usp=sharing" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Dinesh Tamang | AWS Solution Architect | Mumbai, India" />
